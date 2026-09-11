befc4daac4393832020701b4f649af844e56d8a0
BaoNguyen777 <129492539+BaoNguyen777@users.noreply.github.com>
2026-09-11 16:51:55 +0700

    fix: create iframe-free Facebook link card

---
+++ components/FacebookLink.tsx
@@ -0,0 +1,9 @@
+export default function FacebookLink() {
+  return (
+    <a className="facebook-link-card" href="https://www.facebook.com/MeyhomesCapitalPQ/" target="_blank" rel="noreferrer">
+      <div className="facebook-link-art" />
+      <div className="facebook-link-copy"><span>MEYHOMES CAPITAL PHÚ QUỐC</span><small>View the latest stories on Facebook</small></div>
+      <div className="facebook-link-arrow">↗</div>
+    </a>
+  );
+}
