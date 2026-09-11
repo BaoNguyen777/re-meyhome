ef88683ae63f01fe819f3fdeaa733d6ab6030965
BaoNguyen777 <129492539+BaoNguyen777@users.noreply.github.com>
2026-09-11 17:33:32 +0700

    fix: make unused FacebookLink component build-safe

--- components/FacebookLink.tsx
+++ components/FacebookLink.tsx
@@ -1,20 +1,5 @@
-<div className="facebook-card">
-  <div className="fb-top">
-    <span>MEYHOMES</span>
-    <span>FACEBOOK</span>
-  </div>
+import { ArrowUpRight } from "lucide-react";
 
-  <a
-    className="facebook-link-card"
-    href="https://www.facebook.com/MeyhomesCapitalPQ/"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <div className="facebook-link-art" />
-    <div className="facebook-link-copy">
-      <span>MEYHOMES CAPITAL PHÚ QUỐC</span>
-      <small>View the latest stories on Facebook</small>
-    </div>
-    <ArrowUpRight size={22} />
-  </a>
-</div>
+export default function FacebookLink() {
+  return null;
+}
