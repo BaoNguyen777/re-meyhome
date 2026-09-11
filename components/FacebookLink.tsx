0706df7ca6ace3dc22c23778013c17446e03623c
BaoNguyen777 <129492539+BaoNguyen777@users.noreply.github.com>
2026-09-11 17:01:07 +0700

    Update FacebookLink.tsx

--- components/FacebookLink.tsx
+++ components/FacebookLink.tsx
@@ -1,9 +1,20 @@
-export default function FacebookLink() {
-  return (
-    <a className="facebook-link-card" href="https://www.facebook.com/MeyhomesCapitalPQ/" target="_blank" rel="noreferrer">
-      <div className="facebook-link-art" />
-      <div className="facebook-link-copy"><span>MEYHOMES CAPITAL PHÚ QUỐC</span><small>View the latest stories on Facebook</small></div>
-      <div className="facebook-link-arrow">↗</div>
-    </a>
-  );
-}
+<div className="facebook-card">
+  <div className="fb-top">
+    <span>MEYHOMES</span>
+    <span>FACEBOOK</span>
+  </div>
+
+  <a
+    className="facebook-link-card"
+    href="https://www.facebook.com/MeyhomesCapitalPQ/"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <div className="facebook-link-art" />
+    <div className="facebook-link-copy">
+      <span>MEYHOMES CAPITAL PHÚ QUỐC</span>
+      <small>View the latest stories on Facebook</small>
+    </div>
+    <ArrowUpRight size={22} />
+  </a>
+</div>
